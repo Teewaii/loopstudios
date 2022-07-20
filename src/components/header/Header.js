8fd3303849514cb7bcf2c051ae040a4dad709c59
Taiye Ajimati <taiyeajimati@gmail.com>
2022-07-20 09:53:35 +0400

    Hero section complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,12 @@
 import "./Header.css"
-import logo from "../images/logo.png"
+import logo from "../../assets/logo.svg"
+import hamburger from "../../assets/icon-hamburger.svg"
 export default function Header(){
     return(
         
         <div className="hero">
          <nav>
-            <div className="logo"><img src="assets/logo.png" alt="" /></div>
+            <div className="logo"><img src={logo} alt="" /></div>
             <div className="main-nav">
                 <ul>
                     <li><a href="#">About</a></li>
@@ -13,14 +14,20 @@ export default function Header(){
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Products</a></li>
                     <li><a href="#">Support</a></li>
-                </ul>
+                </ul>    
+                            
             </div>
-            <div className="header-ms">
+            <div className="hamburger">
+                    <img src={hamburger} alt="" />
+                    </div>
+         </nav>
+         
+         <div className="header-ms">
                 <h1>Immersive experiences that deliver</h1>
             </div>
-         </nav>
          </div>
     )
 }
+
 
 
