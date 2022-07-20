b28ba5471f9e0c53bb6e201fc45db9b8c61c4eb2
Taiye Ajimati <taiyeajimati@gmail.com>
2022-07-20 18:36:27 +0400

    disabled creations.js

--- src/components/creations/Creations.js
+++ src/components/creations/Creations.js
@@ -1,12 +1,12 @@
 
 import "./Creations.css"
 import React,{useState} from "react"
-import products from "../../assets/creations"
+// import products from "../../assets/creations"
 
 export default function Creations(){
-    const DataArray = products.data.create
-    const creations = DataArray.firstname
-    console.log(creations)
+//     const DataArray = products.data.create
+//     const creations = DataArray.firstname
+//     console.log(creations)
     return(
        
    <div className="creation-container">
