afeaaf4a945f0a858473d59b22f3c42fa323b127
Taiye Ajimati <taiyeajimati@gmail.com>
2022-07-21 12:06:30 +0400

    creations section

---
+++ src/assets/create.js
@@ -0,0 +1,19 @@
+// export default
+
+//  creations= [
+//     {
+//         firstName: "John",
+//         lastName: "Doe"
+//     },
+//     {
+//         firstName: "Anna",
+//         lastName: "Smith"
+//     },
+//     {
+//         firstName: "Peter",
+//         lastName: "Jones"
+//     }
+
+// ]
+
+
