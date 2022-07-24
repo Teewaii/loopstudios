a170dc0e0e8031b4b33f773bce8cd922944c5f68
Taiye <taiyeajimati@gmail.com>
2022-07-25 00:06:11 +0400

    7_25_2022 update

--- src/components/creations/Creations.js
+++ src/components/creations/Creations.js
@@ -1,46 +1,46 @@
-
+ 
 import "./Creations.css"
-import React,{useState} from "react"
+import React, { useState } from "react"
 // import products from "../../assets/creations"
 
-export default function Creations(){
-    return(
-       
-   <div className="creation-container">
-          <h1 className="creation-heading">Our creations</h1>
+export default function Creations() {
+     return (
 
-          <div className="creations-items">
-               <div className="deep-earth card">
-                    <h1 className="title">Deep earth</h1>
-               </div>
-               <div className="Night-arcade card">
-                    <h1 className="title">Night arcade</h1>
-               </div>
-               <div className="soccer card">
-                    <h1 className="title">soccer team vr</h1>
-               </div>
-               <div className="the-grid card">
-                    <h1 className="title">the grid</h1>
-               </div>
-               <div className="from-up card">
-                    <h1 className="title">from up above vr</h1>
-               </div>
-               <div className="pocket card">
-                    <h1 className="title">pocket borealis</h1>
-               </div>
-               <div className="curiosity card">
-                    <h1 className="title">the curiosity</h1>
-               </div>
-               <div className="fisheye card">
-                    <h1 className="title">make it fisheye</h1>
+          <div className="creation-container">
+               <h1 className="creation-heading">Our creations</h1>
+
+               <div className="creations-items">
+                    <div className="deep-earth card">
+                         <h1 className="title">Deep earth</h1>
+                    </div>
+                    <div className="Night-arcade card">
+                         <h1 className="title">Night arcade</h1>
+                    </div>
+                    <div className="soccer card">
+                         <h1 className="title">soccer team vr</h1>
+                    </div>
+                    <div className="the-grid card">
+                         <h1 className="title">the grid</h1>
+                    </div>
+                    <div className="from-up card">
+                         <h1 className="title">from up above vr</h1>
+                    </div>
+                    <div className="pocket card">
+                         <h1 className="title">pocket borealis</h1>
+                    </div>
+                    <div className="curiosity card">
+                         <h1 className="title">the curiosity</h1>
+                    </div>
+                    <div className="fisheye card">
+                         <h1 className="title">make it fisheye</h1>
+                    </div>
+
+                    <div className="see-all">
+                         <button name="see-all">See all</button>
+                    </div>
                </div>
 
-               <div className="see-all">
-                    <button name="see-all">See all</button>
-               </div>
-               </div>
-    
-   </div>
-    )
+          </div>
+     )
 
 }
--- src/components/leader/Leader.js
+++ src/components/leader/Leader.js
@@ -1,22 +1,21 @@
 
 import "./Leader.css"
 
-// import leaderPhoto from "../../assets/mobile/image-interactive.jpg"
 
 
-export default function Leader(){
-    return(
-   <div className="leader-container">
-    <div className="leader-pix">
-        {/* <img src={leaderPhoto} alt="" /> */}
-    </div>
-    <div className="content">
-        <h1 className="leader-header">The leader in interactive vr</h1>
-        <p className="leader-body">
-        Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
-        </p>
-    </div>
-   </div>
+
+export default function Leader() {
+    return (
+        <div className="leader-container">
+            <div className="leader-pix">
+            </div>
+            <div className="content">
+                <h1 className="leader-header">The leader in interactive vr</h1>
+                <p className="leader-body">
+                    Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
+                </p>
+            </div>
+        </div>
     )
 
 }
